44bdaeff35fae6aba492522c4bfef4c615f40a98
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-10-15 11:12:37 -0300

    Adiciona função de buscar objeto do pokemon na api

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ const inputPokemonName = document.getElementById('pokemon-name');
 const searchButton = document.getElementById('btn-search');
 const randomButton = document.getElementById('btn-random');
 
+const renderRates = () => {
+  alert('oi');
+};
+
+const handleSearchEvent = async () => {
+  const pokemonName = inputPokemonName.value;
+  const pokemonObj = await fetchApiPokemon(pokemonName);
+
+  renderRates(pokemonObj);
+};
+
 const addEventSearchButton = () => {
   searchButton.addEventListener('click', handleSearchEvent);
 };
 
 window.onload = () => {
-  
+  addEventSearchButton();
 };
