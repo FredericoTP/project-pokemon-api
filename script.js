48cd2c1b60f147883efa2523af7df14f9162e2de
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-10-15 13:48:52 -0300

    Adiciona as sprites na tela

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 const inputPokemonName = document.getElementById('pokemon-name');
 const searchButton = document.getElementById('btn-search');
 const randomButton = document.getElementById('btn-random');
+const sectionPokemonInfo = document.getElementById('info-pokemon');
 
-const renderRates = () => {
-  alert('oi');
+const renderImagePokemon = (sprites) => {
+  sprites.forEach((sprite) => {
+    const divCard = document.querySelector('.card');
+    const createImg = document.createElement('img');
+    createImg.src = sprite;
+    divCard.appendChild(createImg);
+  });
+};
+
+const renderPokemon = (name, sprites) => {
+  const createDiv = document.createElement('div');
+  createDiv.classList.add('card');
+  sectionPokemonInfo.appendChild(createDiv);
+  renderImagePokemon(sprites);
+};
+
+const pokemonSprites = (sprites) => {
+  const { front_default, front_shiny } = sprites;
+  const sprite = [front_default, front_shiny];
+
+  return sprite;
+}
+
+const pokemonTypes = (types) => types.map((type) => type.type.name);
+
+const renderRates = (pokemonObj) => {
+  const { height, name, sprites, types, weight } = pokemonObj;
+  const type = pokemonTypes(types);
+  const sprite = pokemonSprites(sprites);
+  renderPokemon(name, sprite);
 };
 
 const handleSearchEvent = async () => {
